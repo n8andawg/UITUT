d5823787a12c0dadcaaa4026e3acbf80adcdffad
Nathan <n8andaman2@gmail.com>
2015-11-15 13:15:00 -0500

    New pages for effects, Use button "working"

--- app/src/main/java/com/marrache/nathan/uitut/GainPage.java
+++ app/src/main/java/com/marrache/nathan/uitut/GainPage.java
@@ -6,5 +6,6 @@ import android.support.v7.app.AppCompatActivity;
  * Created by Nathan on 2015-11-11.
  */
 public class GainPage extends AppCompatActivity {
+    //This page should be changing the gain/volume
 
 }
